b9fddf85382fd6e211b1726d0a8d7794c0021fbf
LukeMole <93032440+LukeMole@users.noreply.github.com>
2025-03-30 12:41:17 +0200

    set baseline strength and guidance values and updated requirements.txt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from diffusers import StableDiffusionXLImg2ImgPipeline
 from diffusers.utils import load_image
+import random
 
 pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
     "stabilityai/stable-diffusion-xl-refiner-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
@@ -8,7 +9,15 @@ pipe = StableDiffusionXLImg2ImgPipeline.from_pretrained(
 pipe = pipe.to("mps")
 url = "https://huggingface.co/datasets/patrickvonplaten/images/resolve/main/aa_xl/000000009.png"
 
-init_image = load_image('test.jpg').convert("RGB")
-prompt = "a hyper realistic photo"
-image = pipe(prompt, image=init_image).images
+seed = random.randint(1, 2147483647)
+generator = torch.manual_seed(seed)
+print(seed)
+
+init_image = load_image('test2.jpg').convert("RGB")
+prompt = "a hyper realistic scene"
+# Adjust strength and guidance scale for more creative outputs
+strength = 0.4  # Lower values make the output less like the input image
+guidance_scale = 8  # Higher values make the output more aligned with the text prompt
+
+image = pipe(prompt, image=init_image, strength=strength, guidance_scale=guidance_scale, generator=generator).images
 image[0].save('ai_result.jpg')
