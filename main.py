0c6bc25f4b3d3d1e3a1a63a0955a886f6e4cc292
LukeMole <93032440+LukeMole@users.noreply.github.com>
2025-04-02 09:40:08 +0200

    added audio capturing

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from diffusers import StableDiffusionXLImg2ImgPipeline
 from diffusers.utils import load_image
 import random
 import cv2
-
+import moviepy
+import os
 
 def generate_ai_image(seed, image, strength, guidance_scale, prompt):
     model = "stabilityai/stable-diffusion-xl-refiner-1.0"
@@ -18,10 +19,17 @@ def generate_ai_image(seed, image, strength, guidance_scale, prompt):
     return image[0]
 
 def get_video_data(video_name):
+    #get video
     all_frames = []
     video = cv2.VideoCapture(video_name)
+    resolution = {'x':video.get(cv2.CAP_PROP_FRAME_WIDTH), 'y':video.get(cv2.CAP_PROP_FRAME_HEIGHT)}
+    framerate = video.get(cv2.CAP_PROP_FPS)
     frames = []
     ret, frame = video.read()
+
+    #get audio
+    cur_dir = os.getcwd()
+    audio = moviepy.VideoFileClip(f'{cur_dir}/{video_name}').audio
     #goes through every frame and changes its color, resizes it and appends it to the frames list
     while ret:
         ret, frame = video.read()
@@ -29,10 +37,9 @@ def get_video_data(video_name):
             break
         frames.append(frame)
 
-    framerate = video.get(cv2.CAP_PROP_FPS)
-    resolution = {'x':video.get(cv2.CAP_PROP_FRAME_WIDTH), 'y':video.get(cv2.CAP_PROP_FRAME_HEIGHT)}
-    print(resolution)
-    print(framerate)
+    return {'frames':frames, 'framerate':framerate, 'resolution':resolution, 'audio':audio}
+
+
 
 if __name__ == '__main__':
     strength = 0.4  # Lower values make the output less like the input image 0-1
@@ -43,4 +50,5 @@ if __name__ == '__main__':
 
     #image = generate_ai_image(seed, init_image, strength, guidance_scale, prompt)
     #image.save('ai_result.jpg')
-    get_video_data('test.mp4')
+    video_info = get_video_data('test2.mp4')
+    print(video_info['audio'])
